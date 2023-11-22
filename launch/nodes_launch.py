3a9e0d44751b0dbe543800952dcaf094e09606bf
KrishnaH96 <krishnah@umd.edu>
2023-11-21 21:53:33 -0500

    Added launch file and updated the cmake and package file accordingly

---
+++ launch/nodes_launch.py
@@ -0,0 +1,47 @@
+# /**
+#  * @file nodes_launch.py
+#  * @brief Launch file to launch nodes all together.
+#  *
+#  * This file contains python launch script to launch publisher (talker) node from terminal.
+#  *
+#  * @author Krishna Rajesh Hundekari
+#  * @date 2023-11-21
+#  * @copyright 2023 Krishna Rajesh Hundekari
+# * Apache License Version 2.0, January 2004
+
+#   * Licensed to the Apache Software Foundation (ASF) under one
+#   * or more contributor license agreements.  See the NOTICE file
+#   * distributed with this work for additional information
+#   * regarding copyright ownership.  The ASF licenses this file
+#   * to you under the Apache License, Version 2.0 (the
+#   * "License"); you may not use this file except in compliance
+#   * with the License.  You may obtain a copy of the License at
+#   *
+#   *   http://www.apache.org/licenses/LICENSE-2.0
+#   *
+#   * Unless required by applicable law or agreed to in writing,
+#   * software distributed under the License is distributed on an
+#   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#   * KIND, either express or implied.  See the License for the
+#   * specific language governing permissions and limitations
+#   * under the License.
+#  */
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    publisher_freq_launch = DeclareLaunchArgument(
+        "publish_frequency", default_value='1000'
+    )
+
+    return LaunchDescription([
+        publisher_freq_launch,
+        Node(
+            package='first_ros_package', 
+            executable='talker',
+            parameters=[{"publish_frequency_ms": LaunchConfiguration('publish_frequency')}] # Pass the publish frequency in milliseconds
+        )  
+    ])
